4a8d2c05f8af00e347e70f5f7ffa30e912835898
Ho Thanh Tam <tam.ho@difisoft.com>
2023-07-21 22:57:20 +0700

    test Husky

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const morgan = require('morgan');
+const path = require('path');
+const route = require('./routes/index.js');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+// HTTP logger
+// app.use(morgan("combined"));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+
+app.set('view engine', 'hbs');
+			app.set('views', path.join(__dirname, 'resources/views'));
+
+			// Routes init
+			route(app);
+
+			app.listen(3000);
